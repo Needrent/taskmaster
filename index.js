0a58c7f3480dfb4781588de81ff7205f7edc1d2f
sej <sej@needrent.dk>
2019-10-16 22:57:37 +0200

    added the CSS and JS from the codepen

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Erro World");
+const taskName = document.querySelector("#title");
+const taskNameEdit = document.querySelector('[name="editField"]');
+
+const taskNameBtn = document.querySelector('[data-label="edit"]');
+
+taskNameBtn.addEventListener("click", openEdit);
+
+function openEdit(){
+  taskName.classList.toggle("hide");  
+  taskNameEdit.classList.toggle("hide");
+  
+  taskNameBtn.textContent = "EDIT DONE";
+  taskNameEdit.value = taskName.textContent;
+  
+  taskNameBtn.removeEventListener("click", openEdit);
+  taskNameBtn.addEventListener("click", closeEdit);
+}
+function closeEdit(){
+  taskName.classList.toggle("hide");  
+  taskNameEdit.classList.toggle("hide");
+  
+  taskNameBtn.textContent = "EDIT TASK";
+  taskName.textContent = taskNameEdit.value;
+  
+  taskNameBtn.removeEventListener("click", closeEdit);
+  taskNameBtn.addEventListener("click", openEdit);
+}
+
+
+/*document.querySelector('[data-label="edit"]').addEventListener("click", e =>{
+  let taskName = document.querySelector("#title");
+  let taskNameEdit = document.querySelector('[name="editField"]');
+  
+  taskNameEdit.value = taskName.textContent;
+  taskName.classList.toggle("hide");  
+  taskNameEdit.classList.toggle("hide");
+  taskName.textContent = taskNameEdit.value;
+ 
+});*/
